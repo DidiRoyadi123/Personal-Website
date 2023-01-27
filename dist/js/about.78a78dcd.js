a71c3bf59a272f45095bf0445af5d73a26ec90c5
DidiRoyadi123 <didiroyadi123@gmail.com>
2023-01-27 17:14:16 +0700

    upgrade npm

---
+++ dist/js/about.78a78dcd.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{"754b":function(n,t,u){"use strict";u.r(t);var e=function(){var n=this,t=n._self._c;return t("h1")},l=[],o=u("2877"),s={},a=Object(o["a"])(s,e,l,!1,null,null,null);t["default"]=a.exports}}]);
+//# sourceMappingURL=about.78a78dcd.js.map
